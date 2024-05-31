a1662c6a8d17e7f5e6efbc46334a2e909f919f11
Koen Klinkers <k.klinkers@gmail.com>
2024-05-31 12:11:06 +0200

    created input widget

---
+++ src/input.rs
@@ -0,0 +1,45 @@
+use tui_textarea::TextArea;
+
+use ratatui::style::{Color, Style};
+use ratatui::widgets::{Block, BorderType, Borders};
+use regex::Regex;
+pub struct RegexInput<'a> {
+    pub textarea: TextArea<'a>,
+}
+
+impl RegexInput<'_> {
+    pub fn new() -> Self {
+        let mut textarea = TextArea::default();
+        textarea.set_cursor_line_style(Style::default());
+        textarea.set_placeholder_text("Enter a valid regex");
+
+        textarea.set_block(
+            Block::default()
+                .border_type(BorderType::Rounded)
+                .borders(Borders::ALL),
+        );
+        Self { textarea }
+    }
+
+    pub fn validate(&mut self) {
+        if let Err(err) = Regex::new(&self.textarea.lines()[0]) {
+            self.textarea
+                .set_style(Style::default().fg(Color::LightRed));
+            self.textarea.set_block(
+                Block::default()
+                    .border_type(BorderType::Rounded)
+                    .borders(Borders::ALL)
+                    .border_style(Style::default().fg(Color::LightRed))
+                    .title(format!("{}", err)),
+            );
+        } else {
+            self.textarea
+                .set_style(Style::default().fg(Color::LightGreen));
+            self.textarea.set_block(
+                Block::default()
+                    .border_type(BorderType::Rounded)
+                    .borders(Borders::ALL),
+            );
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+mod input;
+use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+use input::RegexInput;
+use ratatui::backend::CrosstermBackend;
+use ratatui::layout::{Constraint, Layout};
+use ratatui::Terminal;
+use std::io;
+use tui_textarea::{Input, Key};
+
+fn main() -> io::Result<()> {
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
+
+    enable_raw_mode()?;
+    crossterm::execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut term = Terminal::new(backend)?;
+
+    let mut regex_input = RegexInput::new();
+
+    let layout =
+        Layout::default().constraints([Constraint::Length(3), Constraint::Min(1)].as_slice());
+
+    loop {
+        term.draw(|f| {
+            let chunks = layout.split(f.size());
+            let widget = regex_input.textarea.widget();
+            f.render_widget(widget, chunks[0]);
+        })?;
+
+        match crossterm::event::read()?.into() {
+            Input { key: Key::Esc, .. } => break,
+            input => {
+                if regex_input.textarea.input(input) {
+                    regex_input.validate();
+                }
+            }
+        }
+    }
+
+    disable_raw_mode()?;
+    crossterm::execute!(
+        term.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    term.show_cursor()?;
+
+    Ok(())
 }
