34420c175f6c7ecc455b2cbdbd4eaddfc5c9e938
Koen Klinkers <k.klinkers@gmail.com>
2024-05-31 14:54:17 +0200

    some help functionality

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,16 @@ impl Mode {
     }
 }
 
+const HELP: &str = r". Any character    ^ Start of string    $ End of string    * 0+ occurrences       + 1+ occurrences     ? 0 or 1 occurrence
+ {n} Exactly n      {n,} n+ occurrences  {,m} At most m     {n,m} n to m occ.      [abc] Any of a,b,c   [^abc] None of a,b,c
+ [a-z] a to z       [A-Z] A to Z         [0-9] Any digit    \d Any digit           \D Non-digit         \w Word character
+ \W Non-word char   \s Any whitespace    \S Non-whitespace  \t Tab                 \n Newline           \r Carriage return
+ \\ Backslash       \. Literal dot       \+ Literal plus    \* Literal asterisk    \? Literal question  | Alternation
+ (...) Grouping     (?=...) Lookahead    (?!...) Neg lookahead (?<=...) Lookbehind  (?<!...) Neg lookbehind
+ i Case-insensitive g Global search      m Multi-line mode  s Dot matches newline
+
+";
+
 fn main() -> io::Result<()> {
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
@@ -42,21 +52,24 @@ fn main() -> io::Result<()> {
     let mut regex_input = RegexInput::new();
     let mut body = Body::new();
 
+    let banner = Paragraph::new("Regect")
+        .right_aligned()
+        .style(Style::default().fg(Color::Cyan));
+    let help = Paragraph::new(HELP);
     let layout = Layout::default()
         .direction(Direction::Vertical)
         .constraints(vec![
             Constraint::Length(1),
             Constraint::Length(3),
             Constraint::Fill(1),
+            Constraint::Length(7),
         ]);
 
     loop {
         term.draw(|f| {
             let chunks = layout.split(f.size());
-            let banner = Paragraph::new("Regect")
-                .right_aligned()
-                .style(Style::default().fg(Color::Cyan));
-            f.render_widget(banner, chunks[0]);
+            f.render_widget(banner.clone(), chunks[0]);
+            f.render_widget(help.clone(), chunks[3]);
             f.render_widget(regex_input.textarea.widget(), chunks[1]);
             match mode {
                 Mode::RegexEdit => f.render_widget(body.highlighted_body(), chunks[2]),
